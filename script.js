b950076ac896222bbc4b2e6cbdbe9efdc9507103
Tim-Farley <timfarley17@gmail.com>
2025-01-21 15:52:32 +1100

    Completed grid Size Button and tested

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-for (x=0; x<(16*16); x++) {
-    const container = document.querySelector(".container");
+const gridSize = document.querySelector("#setGrid")
+
+gridSize.addEventListener("click", function() {
+    const userInput= parseInt(prompt ("Choose how many sides for the grid (Max. 100):"), 10);
+    if (userInput > 0 && userInput <= 100) {
+        console.log(`Grid size set to: ${userInput} x ${userInput}`);
+         
+
+const container = document.querySelector(".container");
+container.innerHTML = "";
+
+for (x=0; x<(userInput*userInput); x++) {
+    
         const div = document.createElement("div");
+        
+        div.style.width =  `${100 / userInput}%`; 
+        div.style.height = `${100 / userInput}%`; 
+        div.style.boxSizing = "border-box";
+        div.style.border = "1px solid #ddd";
+        
         container.appendChild(div);
+        
         div.addEventListener ("mouseenter", (event) => {  
             event.target.style.backgroundColor = "purple";
                     } )
        
 }
+}
+    else { alert ("Needs to be more than 0 and less that 100")
+}
+})
 
 const clear = document.querySelector("#clear")
 clear.addEventListener("click", () => {
@@ -15,6 +37,7 @@ clear.addEventListener("click", () => {
     div.style.backgroundColor= ""; 
     })
 })
+
 
 
 
